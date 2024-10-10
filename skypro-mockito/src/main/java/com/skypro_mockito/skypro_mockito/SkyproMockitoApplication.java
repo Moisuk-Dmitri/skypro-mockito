abfe8060da633a5824279f21745a64e33456429a
Moisuk-Dmitri <moisuk.d@gmail.com>
2024-10-10 16:27:32 +0300

    Initial commit

---
+++ skypro-mockito/src/main/java/com/skypro_mockito/skypro_mockito/SkyproMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.skypro_mockito.skypro_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproMockitoApplication.class, args);
+	}
+
+}
